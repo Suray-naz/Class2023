dec1b3762ee6b06cf67fd3fdc6d88bb21840b4c7
Suray-naz <129758167+Suray-naz@users.noreply.github.com>
2023-05-09 11:46:21 +0200

    add

--- JS-03-Operators/Comparison Operators/comparison-operators.js
+++ JS-03-Operators/Comparison Operators/comparison-operators.js
@@ -4,4 +4,41 @@ let numOne=3
 let numTwo="3"
 
 console.log(numOne=numTwo);
-console.log(numOne==numTwo);
+console.log("numOne numTwo a denk midir?:", numOne==numTwo);  //true
+// == icinde tuttugu degerleri kiyaslar
+
+console.log("numOne numTwo ya esit midir?:", numOne===numTwo);  //false
+
+let num3="five"
+let num4=5
+
+console.log(num3==num4);
+console.log(num3===num4);
+
+let lang1="Python"
+let lang2="Python"
+
+console.log(lang1==lang2);
+console.log(lang1===lang2);
+
+
+// ? ! (NOT) operatörü -- degil anlami
+// ? !=denk degil
+
+let x=5
+let y="5"
+console.log(x !=y); // false => hayir denktir
+console.log(x !==y); //true => evet bunlar esit degil
+
+5 > 4   //true
+5 > 5   //false
+5>= 5   //true
+
+console.clear();
+console.log(5 > 4);
+console.log(5 > 5);
+console.log(5>= 5);
+
+console.log(10 < 5);
+console.log(10 < 12);
+console.log(10 <= 10);
